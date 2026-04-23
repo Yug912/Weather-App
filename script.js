2bdcdb06475effe392ec1954c58d66570be95385
Yug Thakral <yugthakral07@gmail.com>
2026-04-24 02:29:11 +0530

    changing data on search using js

---
+++ script.js
@@ -0,0 +1,54 @@
+let cityname=document.querySelector('.cityname');
+let searchbtn=document.querySelector('.searchbtn');
+let weatherimage=document.querySelector('.weather-image')
+let temp=document.querySelector('.temp');
+let description=document.querySelector('.description');
+let humidity=document.querySelector('#humidity');
+let windspeed=document.querySelector('#wind-speed');
+
+
+
+async function checkweather(city)
+{
+    const api_key = "acf76042bb8b8db038651fdc9667371b";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+   const weatherdata= await fetch(url).then(response=>response.json());
+    console.log(weatherdata);
+    if(weatherdata.cod === "404")
+    {
+        alert("City not found");
+        return;
+    }
+    temp.innerHTML= `${Math.round(weatherdata.main.temp-273.15)}°C`;
+    description.innerHTML= `${weatherdata.weather[0].description}`;
+    humidity.innerHTML= `${weatherdata.main.humidity}%`;
+    windspeed.innerHTML= `${weatherdata.wind.speed} km/h`;
+
+    switch(weatherdata.weather[0].main)
+    {
+        case 'Clouds':
+            weatherimage.src="cloud.png";
+            break;
+        case 'Clear':
+            weatherimage.src="clear.png";
+            break;
+        case 'Rain':
+            weatherimage.src="rain.png";
+            break;
+        case 'Snow':
+            weatherimage.src="snow.png";
+            break;
+        case 'Mist':
+            weatherimage.src="mist.png";
+            break;
+        case 'Haze':
+            weatherimage.src="Haze.png";
+            break;
+    }
+   
+}
+
+
+searchbtn.addEventListener('click',()=>{
+      checkweather(cityname.value);
+});
